2972434caf39d63125079b0c9b6ebbf072bea38e
raflyandriana14 <rafly.andriana14@gmail.com>
2020-10-05 10:26:58 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charseet="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>Latihan Git Remote </title>
+    </head>
+<body>
+    <h1>Hello world </h1>
+    <p>apa aja boleh</p>
+</body>
+</html>
